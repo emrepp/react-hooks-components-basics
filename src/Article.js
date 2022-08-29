4aa30977a52ea20513b8b9099cd45e7f716f2b34
Emma Repp <emmarepp.draws@gmail.com>
2022-08-29 05:50:42 -1000

    commit tree 083f18b49e0c33fdb5097e3ab2b61723cc9748e3

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,26 @@
 import React from "react";
 
+
+function App() {
+  return (
+    <div>
+      <Article />
+      <Comment />
+    </div>
+  );
+}
+export default App;
+
 function Article() {
   return (
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
   );
 }
+function Comment() {
+  return <div>Naturally, I agree with this article.</div>;
+}
 
-export default Article;
+
+
+
+
